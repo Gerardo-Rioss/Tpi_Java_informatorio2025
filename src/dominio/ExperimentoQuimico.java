03da50be370aa8954542594cabf83cb4fb48a925
Gerardo-Rioss <gerarios26@gmail.com>
2025-10-23 10:53:03 -0300

    Creados las entidades, Experimento, ExperimentoQuimico y Investigador

--- src/dominio/ExperimentoQuimico.java
+++ src/dominio/ExperimentoQuimico.java
@@ -1,4 +1,29 @@
 package dominio;
 
-public class ExperimentoQuimico {
+public class ExperimentoQuimico extends Experimento {
+    private String tipoReactivo;
+    private Investigador investigador;
+
+
+    public ExperimentoQuimico(String nombre, int duracion, Boolean resultado, String tipoReactivo, Investigador investigador) {
+        super(nombre, duracion, resultado);
+        this.tipoReactivo= tipoReactivo;
+        this.investigador= investigador;
+    }
+
+    public String getTipoReactivo() {
+        return tipoReactivo;
+    }
+
+    public void setTipoReactivo(String tipoReactivo) {
+        this.tipoReactivo = tipoReactivo;
+    }
+
+    public Investigador getInvestigador() {
+        return investigador;
+    }
+
+    public void setInvestigador(Investigador investigador) {
+        this.investigador = investigador;
+    }
 }
