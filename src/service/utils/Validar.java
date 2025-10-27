01c8bd1bb51ba0e3945746f900c382cbd4eac3a4
Gerardo-Rioss <gerarios26@gmail.com>
2025-10-27 11:54:24 -0300

    agregado validadores

--- src/service/utils/Validar.java
+++ src/service/utils/Validar.java
@@ -2,8 +2,20 @@ package service.utils;
 
 public class Validar {
     public static void validadNoVacio(String valor, String campo){
-        if(valor==null || valor.trim().isEmpty()){
-            throw new IllegalArgumentException("El cammpo "+campo+" no puede estar vaccío.");
+        if(valor == null || valor.trim().isEmpty()){
+            throw  new IllegalArgumentException("El campo "+ campo+" no puede estar vacio.");
         }
     }
+    public static void validarPositivo(int valor, String campo){
+        if (valor<=0){
+            throw new IllegalArgumentException("El campo "+ campo+ " debe ser un número positivo.");
+        }
+    }
+
+    public static void  validarRango(int valor, int min, int max, String campo){
+        if (valor<min || valor> max){
+            throw new IllegalArgumentException("El campo "+campo+" debe estar entre "+min+" y "+max+".");
+        }
+    }
+
 }
